6b7cf09e94bebe81d11c3806f846d5897826e061
Juan_R <50009426+Juan-RG@users.noreply.github.com>
2020-12-19 11:14:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,15 @@ int main() {
     set<int> s1;
 
     // insert elements in random order
+    /*
+
+
+    */
+     s1.insert(1);
+    s1.insert(2);
+    s1.insert(3);
+    s1.insert(4);
+    s1.insert(5);
     s1.insert(1);
     s1.insert(2);
     s1.insert(3);
