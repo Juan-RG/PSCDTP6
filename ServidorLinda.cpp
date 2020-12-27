287206a3a5078a4022dd62a7613618d68465256d
theri <800616@unizar.es>
2020-12-27 09:38:46 +0100

    Operaciones con lindaDriver RN_2, RdN_2

--- ServidorLinda.cpp
+++ ServidorLinda.cpp
@@ -191,9 +191,9 @@ void servCliente(Socket& soc, int client_fd, MonitorServidor& mS) {
 
             mS.RN_2(tuplaTemp, tuplaTemp2);         //Llamamos a la operacion de servidor
 
-            string tuplaString = tuplaTemp.to_string() + tuplaTemp2.to_string(); //Juntamos las dos tuplas para enviarlas
+            tuplaString1 = tuplaTemp.to_string() + tuplaTemp2.to_string(); //Juntamos las dos tuplas para enviarlas
 
-            send_bytes = soc.Send(client_fd, tuplaString);   //Enviamos la Tupla.
+            send_bytes = soc.Send(client_fd, tuplaString1);   //Enviamos la Tupla.
             if(send_bytes == -1) {
                 cerr << "Error al enviar confirmacion: " + string(strerror(errno)) + "\n";
                 soc.Close(client_fd); // Cerramos los sockets.
@@ -206,194 +206,6 @@ void servCliente(Socket& soc, int client_fd, MonitorServidor& mS) {
 }
 
 int main(int argc, char *argv[]) {
-    /*
-    //Tupla match("prueba1","prueba");
-    Tupla matchG("?Z","prueba");
-    //cout << matchG.match(match)<< "\n";
-    string operacion,tupla;
-    string buffer = "PN,[2,hola,adios,oaooaoa,asdasda,asdasda]";
-    trocea_3(buffer, operacion, tupla);
-    //cout << "buffer: '"<<  buffer << "' operacion '" << operacion << "' tupla '" << tupla << "'" << endl;
-    Tupla t1(4); // TODO: Ver si se puede meter el constructor en from_string, tal que no haya que decir el tamaño de la tupla antes de meterle el string
-    t1.from_string(tupla);
-    //cout << "n tupla " << t1.to_string()<<"\n";
-*/
-    unordered_multiset<Tupla,TuplaHash> almacenPrueba;
-    MonitorServidor mS1(&almacenPrueba);
-    Tupla t1("1","mi␣casa","´arbol"); // 3 elementos
-    Tupla t2("1000");
-    Tupla t3("aprieta","el","pan","45","34","88");
-    Tupla t4("aprieta","fuerte","pan","tt","34","pan");
-
-    mS1.PN(t1);
-    mS1.PN(t1);
-    mS1.PN(t1);
-    mS1.PN(t1);
-    mS1.PN(t1);
-    mS1.PN(t1);
-    cout<<"asdasdada\n";
-    mS1.PN(t2);
-    cout<<"asdasdasdadasdadwwwwwwwwwwwwww\n";
-    mS1.mostrar();
-    mS1.borrar(t1);
-    mS1.mostrar();
-    cout<< "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11\n";
-//muestra "mi casa" por stdout
-    cout << t1.get(2) << endl;
-// t3.to_string() devuelve el string "[aprieta,el,pan,45,34,88]"
-    string serial = t3.to_string();
-    cout << serial << endl;
-// las componentes de t3 tomar´an, respectivamente,
-// los valores "a","b","c","45","34","pan"
-    t3.from_string("[a,b,c,45,34,pan]");
-// mostrar´a [a,b,c,45,34,pan] por stdout
-    cout << t3.to_string() << endl;
-    mS1.PN(t3);
-    cout<< "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!12\n";
-    Tupla t5(3);//0-2
-    cout<< "??????????????????????????\n";
-
-    t5.set(1, "hola");
-    cout<<"******************************\n";
-    t5.set(2, "Mundo");  //1 2 3
-    cout<< "-------------------------------\n";
-    mS1.PN(t5);
-// mostrar´a [,hola,Mundo] por stdout
-    cout << t5.to_string() << endl;
-// informar´a de que tiene 3 elementos
-    cout << "t5␣tiene␣" << t5.size() << "␣elementos" << endl;
-// Un patr´on no es otra cosa que una tupla con
-// la posibilidad de contener el comod´ın "?" en una o m´as posiciones
-// "?" no puede ser un valor de componente de tupla. Para ello se
-// tendr´ıa que representar mediante "??". Creamos dos patrones
-    Tupla p1("?X");
-    Tupla p2("a","?X","c","?Y","34","?Z");
-// Dos nuevas tuplas, de tama˜no 1 y 6, respectivamente
-    Tupla res1(1),res2(p2.size());
-    cout<< "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1\n";
-// ejemplos de RemoveNote
-    mS1.RN(p1); // res1 tomar´a ´el valor que ten´ıa t2
-    mS1.RN(p2); // res2 tomar´a el valor que ten´ıa t3
-    cout << p1.to_string() << endl; //mostrar´a [1000]
-    cout << p2.to_string() << endl; //mostrar´a [a,b,c,45,34,pan]
-
-    cout<< "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!2\n";
-// ¿Si necesitamos un array de tuplas?
-// Tupla v[2]; // NO permitido: no hay constructor por defecto
-    Tupla* v[2];
-    v[0] = new Tupla("Juan", "1000");
-    v[1] = new Tupla("Luisa", "1000", "enero");
-    cout<< "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!3\n";
-    delete v[0];
-    delete v[1];
-
-    Tupla t6("EINA","AOC","DIIS");
-    Tupla t7("EINA","PSCD","DIIS");
-    mS1.PN(t6);
-    mS1.PN(t7);
-    Tupla p3("EINA","?Y","?X");
-    Tupla p4("EINA","?Z","?X");
-    cout<< "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!4\n";
-    Tupla t8(3), t9(3);
-    mS1.RdN_2(p3,p4);
-// Podr´a haber cargado t8 con ["EINA","AOC","DIIS"] y
-// t9 con ["EINA","PSCD","DIIS"], o viceversa
-    cout << p3.to_string() << endl;
-    cout << p4.to_string() << endl;
-
-    Tupla p5("EINA","?Y","?X");
-    Tupla p6("EINA","?Z","?X");
-    mS1.RN_2(p5,p6);
-// Podr´a haber cargado t8 con ["EINA","AOC","DIIS"] y
-// t9 con ["EINA","PSCD","DIIS"], o viceversa
-    cout << p5.to_string() << endl;
-    cout << p6.to_string() << endl;
-
-    //thread p(&prueba1, ref(mS1));
-    //thread p1(&prueba1, ref(mS1));
-    //thread p2(&prueba1, ref(mS1));
-
-
-    //thread p3(&prueba, ref(mS1));
-
-    //thread p4(&prueba, ref(mS1));
-
-    //thread p5(&prueba, ref(mS1));
-
-    //p.join();
-    //p1.join();
-    //p2.join();
-
-
-    //p3.join();
-
-    //p4.join();
-
-    //p5.join();
-
-
-
-
-    //thread p6(&prueba2, ref(mS1));
-
-    //p6.join();
-
-
-   /*
-    Tupla prueba("prueba1","prueba");
-    mS1.PN(match);
-    mS1.PN(matchG);
-    mS1.RdN(prueba);
-*/
-  /*
-    cout << "primera prueba "<< tprueba.match(match)<<"\n";
-    cout << "2 prueba "<< match.match(tprueba)<<"\n";
-    cout << "3 prueba "<< tprueba.match(matchG)<<"\n";
-    cout << "4 prueba "<< matchG.match(tprueba)<<"\n";
-    // PRUEBAS de añadir tuplas al set con el monitor
-
-    string buffer = "PN,[1,zaragoza,valencia,5]";
-    string operacion,tupla;
-    trocea_3(buffer, operacion, tupla);
-    cout << "buffer: '"<<  buffer << "' operacion '" << operacion << "' tupla '" << tupla << "'" << endl;
-    multiset<Tupla> almacenPrueba;
-    MonitorServidor mS1(&almacenPrueba);
-    Tupla t(4); // TODO: Ver si se puede meter el constructor en from_string, tal que no haya que decir el tamaño de la tupla antes de meterle el string
-    t.from_string(tupla);
-    thread p(&prueba1, ref(mS1));
-    thread p1(&prueba1, ref(mS1));
-    thread p2(&prueba1, ref(mS1));
-    mS1.PN(t);
-    mS1.PN(tprueba);
-    mS1.RN(tprueba);
-
-    p1.join();
-    p2.join();
-    p.join();
-    buffer = "PN,[2,teruel,34]";
-    trocea_3(buffer, operacion, tupla);
-    cout << "buffer: '"<<  buffer << "' operacion '" << operacion << "' tupla '" << tupla << "'" << endl;
-    Tupla t1(4); // TODO: Ver si se puede meter el constructor en from_string, tal que no haya que decir el tamaño de la tupla antes de meterle el string
-    t1.from_string(tupla);
-    mS1.PN(t1);
-
-    buffer = "PN,[3,Madrid,La nada,5]";
-    trocea_3(buffer, operacion, tupla);
-    cout << "buffer: '"<<  buffer << "' operacion '" << operacion << "' tupla '" << tupla << "'" << endl;
-    Tupla t2(4); // TODO: Ver si se puede meter el constructor en from_string, tal que no haya que decir el tamaño de la tupla antes de meterle el string
-    t2.from_string(tupla);
-    mS1.PN(t2);
-    mS1.PN(t2);
-    mS1.PN(t2);
-    mS1.PN(t2);
-    mS1.PN(t2);
-    cout<< "Guardo otra vez la tupla\n";
-    buffer = "PN,[3,Madrid,La nada,5]";
-    trocea_3(buffer, operacion, tupla);
-    Tupla t3(4);
-    t3.from_string(tupla);
-    mS1.PN(t);
-    */
     if (argc != 2) {
         cerr << "Número de parámetros incorrecto \n";
         cerr << "Introduce ./ServidorMulticliente, puerto del servidor para hacer bind\n";
@@ -402,22 +214,9 @@ int main(int argc, char *argv[]) {
     const int N = 5;
 
 	//Creamos el tipo de set que vamos a usar (donde guardamos las tuplas)
-    unordered_multiset<Tupla,TuplaHash> almacen;
+    multiset<Tupla> almacen;
     MonitorServidor mS(&almacen);
-    /*
-	MonitorServidor mS(&almacen);
-    Tupla tprueba("prueba1","prueba");
-    Tupla tprueba3("prueba2","prueba");
-    cout << "creada tupla prueba 1" << endl;
-    mS.PN(tprueba2);
-    mS.PN(tprueba);
-    cout << "posteada tupla prueba 1 2 veces" << endl;
-    mS.RdN(tprueba);
-    cout << "leída tupla prueba 1" << endl;
-    //mS1.RdN(tprueba2);
-    cout << "leída tupla inexistente" << endl;
-    // Puerto donde escucha el proceso servidor
-     */
+
     //int SERVER_PORT = atoi(argv[1]);
     int SERVER_PORT = 22;
     thread cliente[N];
